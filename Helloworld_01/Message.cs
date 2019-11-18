1bab8c2158279eaa7cd91c9da6109372264b4ef7
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-18 16:19:02 +0100

    Class Message paramètres optionels.

--- Helloworld_01/Message.cs
+++ Helloworld_01/Message.cs
@@ -9,50 +9,37 @@ namespace Helloworld_01
     class Message
     {
 
-        private void GetHelloMessage(out string salut)
+        private void GetHelloMessage(ref DateTime date, out string salut, int h1 = 9, int h2 = 13, int h3 = 18, int h4 = 24)
         {
-            // Console.WriteLine("\nQuel est ton nom ?");
-            // var name = Console.ReadLine();
             salut = "";
-            var date = DateTime.Now;
-            if (date.ToString("ddd") == "sam." | date.ToString("ddd") == "dim." | date.ToString("ddd") == "ven." & date.Hour >= 18 | date.ToString("ddd") == "lun." & date.Hour < 9)
+            //var date = DateTime.Now;
+            if (date.ToString("ddd") == "sam." | date.ToString("ddd") == "dim." | date.ToString("ddd") == "ven." & date.Hour >= h3 | date.ToString("ddd") == "lun." & date.Hour < h1)
             {
                 salut = "Bon week-end";
-                //Console.WriteLine($"\nBon week-end, {name}, le {date:d} à {date:t} !");
-                // var salut = "Bon week-end";
-                // return Salut;
             }
             else
             {
-                if (date.Hour >= 9 && date.Hour < 13)
+                if (date.Hour >= h1 && date.Hour < h2)
                 {
                     salut = "Bonjour";
-                    // Console.WriteLine($"\nBonjour, {name}, le {date:d} à {date:t} !");
-                    // Salut = "Bonjour";
-                    // return Salut;
                 }
-                else if (date.Hour >= 13 && date.Hour < 18)
+                else if (date.Hour >= h2 && date.Hour < h3)
                 {
                     salut = "Bon après-midi";
-                    // Console.WriteLine($"\nBon après-midi, {name}, le {date:d} à {date:t} !");
-                    // Salut = "Bon après-midi";
-                    // return Salut;
                 }
-                else if (date.Hour >= 18 && date.Hour < 24 || date.Hour >= 0 && date.Hour < 9)
+                else if (date.Hour >= h3 && date.Hour < h4 || date.Hour >= 0 && date.Hour < h1)
                 {
                     salut = "Bon soir";
-                    // Console.WriteLine($"\nBon soir, {name}, le {date:d} à {date:t} !");
-                    // Salut = "Bon soir";
-                    // return Salut;
                 }
             }
         }
 
         public void ShowMessage()
         {
-            string salut;
-            GetHelloMessage(out salut);
+            //string salut;
             var date = DateTime.Now;
+
+            GetHelloMessage(ref date, out string salut);
 
             Console.WriteLine("\nQuel est ton nom ?");
             var name = Console.ReadLine();
