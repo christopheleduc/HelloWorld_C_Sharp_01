abe38affcbb8ceb59917e40fcfd8c2b9ef2c4e1d
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-19 17:02:54 +0100

    Waiting to next...

---
+++ Helloworld_01/Date.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld_01
+{
+    internal class Date : IDate
+    {
+        public DateTime GetDate()
+        {
+            //throw new NotImplementedException();
+            DateTime date = DateTime.Now;
+            return date;
+        }
+    }
+}
