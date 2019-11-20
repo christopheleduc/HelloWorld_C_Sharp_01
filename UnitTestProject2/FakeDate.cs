42975d8f369f3da292796396bf0cc34526114501
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-20 13:17:07 +0100

    Interfaces en cours: non implémentés...

---
+++ UnitTestProject2/FakeDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Helloworld_01;
+
+namespace UnitTestProject2
+{
+    internal class FakeDate : IDate
+    {
+        public DateTime GetDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
